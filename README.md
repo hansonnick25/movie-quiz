4bade5a4abbc4107e9062b5cee6553458a994644
Nick Hanson <hansonnick25@gmail.com>
2023-07-25 21:00:36 -0500

    Finalizing project idea

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # movie-quiz
+
+Acceptance Criteria
+GIVEN I am playing the movie-quiz game
+WHEN I load the homepage
+THEN it loads properly
+WHEN I press Start
+THEN the game begins
+AND the first question pops up
+AND I am presented with a Rotten Tomatoes (or IMDB) score and a list of 4 movies that it could possibly be
+WHEN I select an answer
+AND press Submit
+THEN a review page pops up with a link to the movie trailer, a fun fact about the movie and whether or not you got the question right
+WHEN you hit Continue
+THEN the next question loads
+OR the end of the quiz if you are at the final question
+WHEN the end page of the quiz loads
+THEN I see a list of High scores from LocalStorage
---
+++ assets/js/script.js
@@ -0,0 +1,43 @@
+// hard coded movie IDs for now, eventually will be pulled from the MDB API
+let movieId = [
+  "tt0108002",
+  "tt0112453",
+  "tt0123755",
+  "tt0107034",
+  "tt0119822",
+  "tt0147800",
+  "tt0099850",
+  "tt0100828",
+  "tt0120791",
+  "tt0115678",
+];
+
+// This needs to go first
+const mdbSettings = {
+  async: true,
+  crossDomain: true,
+  url: "https://moviesdatabase.p.rapidapi.com/titles/random?startYear=1930&genre=Drama&endYear=1980&list=most_pop_movies",
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "e6f6f362ebmsh767ffbd1821e970p1c3b80jsn8ca07780167f",
+    "X-RapidAPI-Host": "moviesdatabase.p.rapidapi.com",
+  },
+};
+
+$.ajax(mdbSettings).done(function (response) {
+  console.log(response);
+});
+
+// This needs to fire after the above ajax call is done ( will figure out later )
+const omdbSettings = {
+  async: true,
+  crossDomain: true,
+  url: `http://www.omdbapi.com/?i=${movieId[1]}&apikey=1a17fd2d`,
+  method: "GET",
+};
+
+$.ajax(omdbSettings).done(function (response) {
+  console.log(`omdb stuff`);
+  console.log(response);
+  console.log(response.Ratings[1].Value);
+});
